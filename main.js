8a1d26dc338c61ab82481897032394adc51808cc
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2019-12-11 22:12:15 +0200

    getting elements, defining debug functions

---
+++ main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const startButton = document.getElementById('startButton');
+const callButton = document.getElementById('callButton');
+const hangupButton = document.getElementById('stopButton');
+callButton.disabled = true;
+hangupButton.disabled = true;
+startButton.addEventListener('click', start);
+callButton.addEventListener('click', call);
+hangupButton.addEventListener('click', stop);
+let startTime;
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+
+async function start() {
+    console.log('Start')
+};
+
+async function call() {
+    console.log('Calling')
+};
+
+async function stop() {
+    console.log('Stop')
+};
+
+
+/// Debug functions
+
+localVideo.addEventListener('loadedmetadata', function() {
+    console.log(`Local video videoWidth: ${this.videoWidth}px,  videoHeight: ${this.videoHeight}px`);
+});
+
+remoteVideo.addEventListener('loadedmetadata', function() {
+    console.log(`Remote video videoWidth: ${this.videoWidth}px,  videoHeight: ${this.videoHeight}px`);
+});
+
